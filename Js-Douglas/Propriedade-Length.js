369a4a74ff5f9215d79496b9d1a468cfc91e3f72
nondrack <luckasfernando500@gmail.com>
2025-05-03 16:27:15 -0300

    estudos de array

---
+++ Js-Douglas/Propriedade-Length.js
@@ -0,0 +1,30 @@
+// const frutas = ["maçã", "banana", "laranja"];
+// console.log(frutas.length);
+
+const prompt = require("prompt-sync")();
+
+// let nomes = []
+// let notas = []
+// let ctn = ""
+// do {
+//     let nome = prompt("Digite um Nome: ");
+//     let continuar = prompt("Quer digitar mais nomes: s/n ? ");
+
+//     ctn = continuar 
+
+//     nomes.push(nome);
+
+// }while (ctn == "s");
+
+// console.log("A quantidade de nomes foram: " + nomes.length);
+
+// for(let x=0; x< nomes.length; x++){
+//     let nota = prompt("A nota do aluno: " + (nomes[x]) + " = ");
+//     notas.push(nota)
+    
+// }
+
+
+
+
+
---
+++ Js-Douglas/pratica.js
@@ -0,0 +1,36 @@
+const prompt = require("prompt-sync")();
+
+// subtraia ou adicione nomes na array com .splice(indice, quantidade de string removido ou substituido, informação adicionada)
+
+// let nomes =  ["lucas","fernando","ana"]
+
+// let x = 0
+// do {
+//     let informados = prompt("Digite Um nome : ");
+
+//     nomes.splice(1, 2,"Amanda");
+
+//     x = x+1
+
+// }while(x < 1 )
+//     console.log(nomes);
+
+// adicione nomes ao inicio do array com .unshift(informação a ser adiciionada)
+
+// let nomes = ["Lucas","Fernando","Barbosa"]
+
+// let nome = prompt("Digite um Nome: ");
+
+// nomes.unshift(nome)
+// console.log(nomes);
+
+// adicione objetos no final de uma array com o .push(informação a ser adicionada)
+
+// let nomes = ["lucas","adriana","jaque","Divino"]
+
+// let informados = prompt("Digte um nome: ");
+
+// nomes.push(informados);
+
+// console.log(nomes);
+
