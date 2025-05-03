a7d10a6e6c30c7eb7048dd80a2162e0c66d3adef
nondrack <luckasfernando500@gmail.com>
2025-05-03 18:11:50 -0300

    atualização da aula

--- Js-Douglas/Propriedade-Length.js
+++ Js-Douglas/Propriedade-Length.js
@@ -21,10 +21,45 @@ const prompt = require("prompt-sync")();
 // for(let x=0; x< nomes.length; x++){
 //     let nota = prompt("A nota do aluno: " + (nomes[x]) + " = ");
 //     notas.push(nota)
-    
+// }
+// for(let x=0; x<nomes.length; x++){
+//     console.log("Nota: " + nomes[x] + " -> " + notas[x])
 // }
 
 
 
+// const frutas = ["maçã", "banana", "laranja"];
+// const ultimaFruta = frutas.pop();
+// console.log(ultimaFruta);
+// console.log(frutas); 
 
 
+// const livros = ["harry potter", "the wither", "World of warcraft", "subnautica", "the last of us", "legion", "Eli"]
+
+// for(let x=0; x <7; x++){
+//     let livrosRestantes = prompt("Os livros restantes são: " + livros)
+//     const ultimoLivro = livros.pop();
+// }
+// console.log("Acabaram os lvros")
+
+// const frutas = ["maçã", "banana"];
+// const novoTamanho = frutas.push("laranja", "uva");
+// console.log(frutas);
+// console.log(novoTamanho); 
+
+// let lista = []
+// let adcTarefa = ""
+
+// do{
+    
+//     let tarefa = prompt("Adicionar uma tarefa: ");
+//     lista.push(tarefa)
+//     console.log(lista)
+//     adcTarefa = prompt("Quer adicionar uma tarefa? s/n ");
+
+// }while(adcTarefa == "s")
+//     for(let x = 0; x < lista.length; x++){
+//         console.log(x + 1 + " -> " + lista[x]);
+        
+//     }
+    
