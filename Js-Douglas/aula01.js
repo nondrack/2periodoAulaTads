20c2b2a5c9d51d852c3adbecdf962f062e4bf71e
nondrack <luckasfernando500@gmail.com>
2025-05-03 14:09:31 -0300

    ex aula de array

--- Js-Douglas/aula01.js
+++ Js-Douglas/aula01.js
@@ -6,7 +6,7 @@
 
 // for (let x=0; x<7; x++){
 //     console.log(numeros[x]);
-    
+
 // }
 const prompt = require("prompt-sync")();
 
@@ -18,20 +18,49 @@ const prompt = require("prompt-sync")();
 
 // media = media / 6
 // console.log('media: ', media);
-let soma = 0
-let contador = 0
-let numeros = [3,-2,5,-9,5,1,-7,8,5,10]
 
-for (x = 0; x <10; x++){
-    if(numeros[x] < 0){
-        contador = contador +1
-    }else{
-        soma = soma +numeros[x]
-    }
-}
-console.log("A quantidade de numeros negativos são: " + contador);
-console.log("A soma dos numeros positivos é:  " + soma);
+// let soma = 0
+// let contador = 0
+// let numeros = [3,-2,5,-9,5,1,-7,-8,7,10]
 
+// for (x = 0; x <10; x++){
+//     if(numeros[x] < 0){
+//         contador = contador +1
+//     }else{
+//         soma = soma +numeros[x]
+//     }
+// }
+// console.log("A quantidade de numeros negativos são: " + contador);
+// console.log("A soma dos numeros positivos é:  " + soma);
+
+// let numeros = [3, 60, 6, 2, 88, 4, 75, 20, 9, 76];
+// let numeroDigitado = Number(prompt("Digite um numero: "));
+// for (let x = 0; x < 10; x++) {
+//   if (numeros[x] == numeroDigitado) {
+//     console.log("A posição do nuemro " + numeroDigitado + " é " + x);
+//   }
+// }
+
+
+// let nomes = []
+// let quantidade = 0
+// let otroNome = ""
+// do {
+//     let nome = prompt("Digite um Nome: ");
+//     let continuar = prompt("Quer digitar mais nomes: s/n ? ");
+
+//     quantidade = quantidade +1
+//     otroNome = continuar 
+
+//     nomes.push(nome);
+
+// }while (otroNome == "s");
+
+
+
+// console.log("A quantidade de nosmes digitados foi: " + quantidade);
+
+// console.log("Os nomes informados foram : " + nomes);
 
 
 
